0bb09595626b35fe3a404ba34b22e44c7b68e86f
grclaw <grclaw@local.invalid>
2026-04-13 16:38:49 +0000

    Align unit tests with current lesson model

--- GreatsOfBharathaTests/GreatsOfBharathaTests.swift
+++ GreatsOfBharathaTests/GreatsOfBharathaTests.swift
@@ -2,14 +2,17 @@ import XCTest
 @testable import Greats_Of_Bharatha
 
 final class GreatsOfBharathaTests: XCTestCase {
-    func testSampleContentHasShivajiArc() {
-        let store = ShivajiLessonStore()
-        XCTAssertFalse(store.scenes.isEmpty)
-        XCTAssertEqual(store.scenes.first?.title, "Scene 1: Birth at Shivneri")
+    func testSampleContentHasScenes() {
+        let model = AppModel()
+        XCTAssertFalse(model.content.scenes.isEmpty)
+        XCTAssertEqual(model.content.scenes.first?.title, "Birth at Shivneri")
     }
 
-    func testAppModelStartsOnStoryTab() {
-        let model = AppModel()
-        XCTAssertEqual(model.selectedTab, .story)
+    func testLessonStoreTracksProgress() {
+        let store = ShivajiLessonStore()
+        XCTAssertEqual(store.completedScenes, 0)
+        store.markScene("scene-1-shivneri", mastery: .witnessed)
+        XCTAssertEqual(store.completedScenes, 1)
+        XCTAssertNotNil(store.nextSceneID)
     }
 }
