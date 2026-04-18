be5af8a0b599a63892d0517101969d7eb45fcec1
grclaw <grclaw@local.invalid>
2026-04-18 02:59:55 +0000

    feat: prototype haptic lesson feedback

---
+++ GreatsOfBharatha/Shared/Theme/LessonFeedback.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct LessonFeedback {
+    enum HapticToken {
+        case selection
+        case reveal
+        case success
+        case warning
+    }
+
+    static func fire(_ token: HapticToken) {
+#if os(iOS)
+        switch token {
+        case .selection:
+            UISelectionFeedbackGenerator().selectionChanged()
+        case .reveal:
+            let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.prepare()
+            generator.impactOccurred()
+        case .success:
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+        case .warning:
+            UINotificationFeedbackGenerator().notificationOccurred(.warning)
+        }
+#else
+        _ = token
+#endif
+    }
+}
